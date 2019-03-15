673df070021dedb37023d49efbd7db0a85645bdc
菜鳥祐 <a9650615@gmail.com>
2019-03-15 20:00:23 +0800

    🎉Create first express application with hot reload

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import PrettyError from 'pretty-error';
+
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+const prettyError = new PrettyError();
+prettyError.skipNodeFiles();
+prettyError.skipPackage('express');
+
+app.use((err, req, res, next) => {
+  process.stderr.write(prettyError.render(err));
+  next();
+});
+
+export default app;
